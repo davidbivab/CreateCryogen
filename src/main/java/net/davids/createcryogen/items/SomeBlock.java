ecc01b0b30b8935661ffe346313bd1bf608b4b37
davidbivab <yanada611@gmail.com>
2024-02-25 21:51:03 +0300

    main

---
+++ src/main/java/net/davids/createcryogen/items/SomeBlock.java
@@ -0,0 +1,9 @@
+package net.davids.createcryogen.items;
+
+import net.minecraft.world.level.block.Block;
+
+public class SomeBlock extends Block {
+    public SomeBlock(Properties properties) {
+        super(properties);
+    }
+}
